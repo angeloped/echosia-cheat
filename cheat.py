ffa1f3a8438576cb8aabdcbd28c5f98c1796e0da
angeloped <bryanpedrosa123@gmail.com>
2019-08-25 11:18:47 +0800

    edited structure, added score logger

--- cheat.py
+++ cheat.py
@@ -2,17 +2,21 @@ import random
 import urllib
 import datetime
 import string
-import time
+#import time; no delay to speed up
 
 total_trees = 0
-while 1:
-	time_today = datetime.datetime.today().strftime("%m/%d/%y")
-	random_query = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(random.randint(10,50)))
-	response_ecosia = urllib.urlopen("https://www.ecosia.org/search?q={0}".format(random_query)).read().split("<span class=\"pill-text tree-counter-text js-treecount-text\">")[1].split("</span>")[0].replace(" ","").replace("\n","")
-	response_ecosia = int(response_ecosia)
-	if response_ecosia >= 1:
-		total_trees += 1
-		print("[{0}] total_record: {1}; search_query: {2}".format(time_today, total_trees, random_query))
-	else:
-		print("[{0}] increment freezes! you've added {1} trees to be planted.".format(time_today, total_trees))
-	time.sleep(1)
+try:
+	while 1:
+		time_today = datetime.datetime.today().strftime("%m/%d/%y")
+		random_query = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(random.randint(10,50)))
+		response_ecosia = urllib.urlopen("https://www.ecosia.org/search?q={0}".format(random_query)).read().split("<span class=\"pill-text tree-counter-text js-treecount-text\">")[1].split("</span>")[0].replace(" ","").replace("\n","")
+		response_ecosia = int(response_ecosia)
+		if response_ecosia >= 1:
+			total_trees += 1
+			print("[{0}] total_record: {1}; search_query: {2}".format(time_today, total_trees, random_query))
+		else:
+			print("[{0}] increment freezes! you've added {1} trees to be planted.".format(time_today, total_trees))
+		#time.sleep(1); no delay to speed up
+except:
+	with open("scores.log","a+") as score:
+		score.write("{0}\n".format(total_trees))
